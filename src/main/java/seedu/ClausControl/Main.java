48c97adae8df436991e81e4394870578e81de670
shrabasti <shrabastichakraborty16@gmail.com>
2026-03-13 10:40:56 +0530

    Update logo and class names

---
+++ src/main/java/seedu/ClausControl/Main.java
@@ -0,0 +1,22 @@
+package seedu.ClausControl;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String LOGO = "  .-\"\"-.\n" +
+            " /,..___\\\n" +
+            "() {_____}\n" +
+            "  (/-@-@-\\)\n" +
+            "  {`-=^=-'}\n" +
+            "  {  `-'  } Welcome to ClausControl, Santa!\n" +
+            "   {     }\n" +
+            "    `---'\n";
+
+    /**
+     * Main entry-point for the ClausControl application.
+     */
+    public static void main(String[] args) {
+        System.out.println(LOGO);
+    }
+}
